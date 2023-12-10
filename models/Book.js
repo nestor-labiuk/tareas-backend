e77f70abf9f36b5c27376c5a6d1e07bcd841fc99
nestor-labiuk <nestorlabiuk@gmail.com>
2023-12-10 19:39:16 -0300

    Agrega eslint y modelo de libros

--- models/Book.js
+++ models/Book.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
 
 const bookSchema = new mongoose.Schema({
-    book: {type: 'string', required: true}
+    book: {type: String, required: true},
+    gender: {type: String, required: true},
+    price: {type: Number, required: true}
 })
 
 const book = mongoose.model('Book', bookSchema)
