d2169f74fff4dad1cddb9052370608bd07546b65
nestor-labiuk <nestorlabiuk@gmail.com>
2023-12-16 20:02:01 -0300

    limpia archivo app.js y translsdo el codigo a los archivos de rutas y controladores de autores y libros

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const booksRouter = require('express').Router
 const { getAllBooks, getBookById, createBook, updateBook, deleteBook } = require('../controllers/books.controllers')
 
@@ -11,3 +12,17 @@ booksRouter.delete('/:id', deleteBook)
 module.exports = booksRouter
 
 
+=======
+const { getAllBooks, getBookById, createBook, updateBook, deleteBook } = require('../controllers/books.controllers')
+
+const booksRouter = require('express').Router()
+
+
+booksRouter.get('/', getAllBooks) 
+booksRouter.get('/:id', getBookById) 
+booksRouter.post('/', createBook) 
+booksRouter.put('/:id', updateBook) 
+booksRouter.delete('/:id', deleteBook) 
+
+module.exports = booksRouter
+>>>>>>> c41eb02 (limpia archivo app.js y translsdo el codigo a los archivos de rutas y controladores de autores y libros)
